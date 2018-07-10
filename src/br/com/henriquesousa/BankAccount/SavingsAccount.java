faf09ebe89cf1a5270247e79ef6ff47889378619
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2018-07-10 09:47:12 -0300

    added savings account

--- src/br/com/henriquesousa/BankAccount/SavingsAccount.java
+++ src/br/com/henriquesousa/BankAccount/SavingsAccount.java
@@ -1,35 +1,32 @@
 package br.com.henriquesousa.BankAccount;
 
-public class Account {
-	protected String name;
-    protected String ID;
-    //todo add password
-    protected double balance = 0;
+public class SavingsAccount extends Account {
+	private int numberOfWithdrawals = 2;
+	private int numberOfTranfsers = 2;
+	//todo add password
 
-    public double minimumWithdrawal;
-    public double maximumWithdrawal;
-
-    public Account(String name, String ID) {
-    	this.name = name;
-    	this.ID = ID;
-    }
-    
+	public SavingsAccount(String name, String ID) {
+	 	super(name, ID);
+	}
+	
     @Override
     public String toString() {
     	return "Name: " + name + "\n" +
     			"ID: " + ID + "\n" +
     			"Balance: " + balance + "\n" +
-    			"Type: Checking Account";
+    			"Type: Savings Account";
     }
-    
+    @Override
     public double deposit(){
     	//todo
         return 0;
     }
+    @Override
     public void withdraw(){
     	//todo
         return;
     }
+    @Override
     public void transferToAccount(Account otherAccount){
     	//todo
         return;
--- src/br/com/henriquesousa/Main.java
+++ src/br/com/henriquesousa/Main.java
@@ -1,32 +1,51 @@
 package br.com.henriquesousa;
 
 import br.com.henriquesousa.BankAccount.Account;
+import br.com.henriquesousa.BankAccount.SavingsAccount;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args){
 		int option;
+		List<Account> accountsList = new ArrayList<Account>();
+		List<SavingsAccount> savingsAccountsList = new ArrayList<SavingsAccount>();
 		
 		do {
 	        System.out.println(
-	        "Menu" + "\n" +
-	        "1. Create account" + "\n" +
-	        "0. Exit" + "\n"
+		        "Menu" + "\n" +
+		        "1. Create account" + "\n" +
+		        "0. Exit" + "\n"
 	        );
 	        Scanner input = new Scanner(System.in);
 	        option = input.nextInt();
 	        switch(option) {
 	        	case 1: 
+	        		System.out.println(
+	        				"What type of account you want to create?" + "\n" + 
+	        				"1. Checking account" + "\n" + 
+	        				"2. Savings account" + "\n"
+	        		);
+	        		int option2 = input.nextInt();
 	        		System.out.println("Type your name: ");
 	        		String name = input.next();
 	        		System.out.println("Type your ID: ");
 	        		String ID = input.next();
-	        		Account newAccount = new Account(name, ID);
-	        		System.out.println("Account created \n" + newAccount.toString());
-	        		
+	        		switch(option2) {
+	        			case 1: 
+	        				Account newAccount = new Account(name, ID); 
+	        				System.out.println("Account created \n" + newAccount.toString());
+	        				break;
+	        			case 2: 
+	        				Account newSavingsAccount = new SavingsAccount(name, ID); 
+	        				System.out.println("Account created \n" + newSavingsAccount.toString());
+	        				break;
+	        		}
 	        		break;
 	        }
+	        input.close();
 		} while(option != 0);
-        
 	}
 }
